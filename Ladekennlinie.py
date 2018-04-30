2ef19a9ff19e3b699c4afe6b1c221c7fb86b6d37
SuperPlusUser <habliz.jannik@online.de>
2018-05-01 00:02:13 +0200

    Ladekennlinie mit BattMon

--- Ladekennlinie.py
+++ Ladekennlinie.py
@@ -1,23 +1,29 @@
-import Sensorik_async as Sensorik
+import Sensorik
 import time
 import asyncio
 
-start = time.time()
 loop = asyncio.get_event_loop()
-
 Sensorik.init(loop)
 
-try:
-    V = Sensorik.INA219_Voltage()
-    I = Sensorik.INA219_Current()
+V = Sensorik.Batt_Mon_Voltage()
+I = Sensorik.Batt_Mon_Current()
+C = Sensorik.Batt_Mon_Charge()
 
-    v = open('Entladekennlinie_Voltage', 'w')
+start = time.time()
+
+try:
+    v = open('Ladekennlinie_Voltage', 'w')
     v.write("time, voltage[V]\n")
     V.subscribe((lambda n,d,u : v.write(str(time.time() - start) +", " + d + "\n")), False, 10)
     
-    i = open('Entladekennlinie_Current', 'w')
+    i = open('Ladekennlinie_Current', 'w')
     i.write("time, current[A]\n")
     I.subscribe((lambda n,d,u : i.write(str(time.time() - start) +", " + d + "\n")), False, 10)
+    
+    c = open('Ladekennlinie_Charge', 'w')
+    c.write("time, charge[mAh]\n")
+    start_charge = float(C.getSensorData())
+    C.subscribe((lambda n,d,u : c.write( str(time.time() - start) +", " + str(float(d) - start_charge) + "\n")), False, 10)
     
     loop.run_forever()
     
@@ -25,4 +31,5 @@ finally:
     print("Saving Files...")
     v.close()
     i.close()
+    c.close()
     Sensorik.close()
